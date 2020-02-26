055cfd31b9952bb8d8d3483ba3f79f0e701b27aa
Edson Ayllon <eayllon1@gmail.com>
2020-02-25 23:26:01 -0600

    final for now

--- src/main/java/object/notification/java/ObjectA.java
+++ src/main/java/object/notification/java/ObjectA.java
@@ -1,8 +1,37 @@
 package object.notification.java;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.Set;
+import object.notification.java.ObjectB;
+
 public class ObjectA {
-    public String getGreeting() {
-        return "Hello world 2.";
+    private int THRESHOLD;
+    private int value;
+    private ObjectB objB;
+
+
+    ObjectA(ObjectB objB) {
+        this.objB = objB;
+        Properties props = new Properties();
+        InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties");
+        try {
+            props.load(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        this.THRESHOLD = Integer.parseInt(props.getProperty("THRESHOLD"));
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+        if (this.value > THRESHOLD) this.notification();
+    }
+
+    private void notification() {
+        this.objB.notifying();
     }
 }
 
--- src/main/java/object/notification/java/ObjectB.java
+++ src/main/java/object/notification/java/ObjectB.java
@@ -1,8 +1,28 @@
 package object.notification.java;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class ObjectB {
-    public String getGreeting() {
-        return "Hello world 2.";
+    private String NOTIFICATION_MESSAGE;
+    private Properties props = new Properties();
+
+    ObjectB() {
+        Properties props = new Properties();
+        InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties");
+        try {
+            props.load(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        this.NOTIFICATION_MESSAGE = props.getProperty("NOTIFICATION_MESSAGE");
+    }
+
+    public String notifying() {
+        System.out.println(this.NOTIFICATION_MESSAGE);
+        return this.NOTIFICATION_MESSAGE;
     }
 }
 
